6453dd9616eec070e55256f4f9290657ada8062a
maritimeseamanship <ekaterinataer@gmail.com>
2023-08-04 18:58:24 +0300

    initial commit hw2_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react';
+import {Launches} from './components/Launches/Launches';
 
 const App = () => {
   return (
     <div className="App">
-
+               <Launches/>
     </div>
   );
 };
 
-export default App;
+export {App};
